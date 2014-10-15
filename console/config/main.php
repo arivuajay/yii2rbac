36e17ec354dad8fa5e82930e6887c5cc0ac1cf99
Arivu Ajay <arivu.sacs@gmail.com>
2014-10-15 15:18:16 +0530

    Iniitial Commit

---
+++ console/config/main.php
@@ -0,0 +1,45 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-console',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log', 'gii'],
+    'controllerNamespace' => 'console\controllers',
+    'modules' => [
+        'gii' => 'yii\gii\Module',
+        'auth' => [
+                'class' => 'auth\Module',
+                'superAdmins' => ['admin'], // SuperAdmin users
+        ],
+    ],
+    'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager'
+        ],
+        'user' => [
+            'class' => 'auth\components\User',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+    ],
+    'params' => $params,
+];
